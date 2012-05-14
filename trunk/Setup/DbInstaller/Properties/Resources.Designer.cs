364fc585c2e675e3ccb3635a5a56d1477dc66bd8
vajanko@gmail.com <vajanko@gmail.com@98fb08ef-915f-c79d-25f9-955841894933>
2012-05-14 08:17:14 +0000

    - namespace fix

--- trunk/Setup/DbInstaller/Properties/Resources.Designer.cs
+++ trunk/Setup/DbInstaller/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Praetor.Setup.Properties {
+namespace MTS.Setup.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Praetor.Setup.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Praetor.Setup.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MTS.Setup.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- trunk/Setup/DbInstaller/Properties/Settings.Designer.cs
+++ trunk/Setup/DbInstaller/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Praetor.Setup.Properties {
+namespace MTS.Setup.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
